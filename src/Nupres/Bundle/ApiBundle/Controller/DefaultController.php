cca4639f8e834d9d90534364b7c01a5f48ae7394
Diego Torres <diegotorres50@hotmail.com>
2017-11-26 16:01:23 -0500

    Implementamos cliente de mysql para hacer consultas a la base de datos usando un modelo propio

--- src/Nupres/Bundle/ApiBundle/Controller/DefaultController.php
+++ src/Nupres/Bundle/ApiBundle/Controller/DefaultController.php
@@ -3,11 +3,23 @@
 namespace Nupres\Bundle\ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+use Nupres\Bundle\ApiBundle\Model\DataBase\MysqlClient;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('NupresApiBundle:Default:index.html.twig');
+        $_mysqlClient = MysqlClient::getInstance(array());
+        $_results = $_mysqlClient->rawQuery('SELECT * FROM informe_cuidado_critico_general;');
+
+        $response = new Response();
+        $response->setContent(json_encode(array(
+            'data' => $_results,
+        )));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
     }
 }
---
+++ src/Nupres/Bundle/ApiBundle/Model/DataBase/MysqlClient.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Nupres\Bundle\ApiBundle\Model\DataBase;
+
+use \MysqliDb;
+
+class MysqlClient
+{
+
+    private $_client = null;
+
+    private static $_instance; // The singleton instance
+
+    /*
+    Get an instance of the Mysql
+    @return Instance
+    */
+    public static function getInstance($arguments = [])
+    {
+        if (!self::$_instance) { // If no instance then make one
+            self::$_instance = new self($arguments);
+        }
+
+        return self::$_instance;
+    }
+
+    public function __construct($arguments = [])
+    {
+        try {
+            $this->_client = new MysqliDb('127.0.0.1', 'root', 'Colombia2006', 'nupres_dev_demo01');
+        } catch (\Exception $ex) {
+            throw $ex;
+        }
+    }
+
+    // Magic method clone is empty to prevent duplication of client
+    private function __clone()
+    {
+    }
+
+    // Get mysql client
+    public function getClient()
+    {
+        return $this->_client;
+    }
+
+    // Exec raw query
+    public function rawQuery($query)
+    {
+        return $this->_client->rawQuery($query);
+    }
+}
